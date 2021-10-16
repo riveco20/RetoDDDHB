0d1678433b2c72c300dbd9b69eb22975c2c29c38
Richard Vellojin Coneo <richardvellojin@gmail.com>
2021-10-15 23:55:23 -0500

    Agregando archivo inicial

---
+++ src/main/java/com/hojadevida/retoHojaDeVida/RetoHojaDeVidaApplication.java
@@ -0,0 +1,13 @@
+package com.hojadevida.retoHojaDeVida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoHojaDeVidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoHojaDeVidaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hojadevida/retoHojaDeVida/RetoHojaDeVidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hojadevida.retoHojaDeVida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoHojaDeVidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
